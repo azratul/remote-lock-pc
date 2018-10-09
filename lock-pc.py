df8b6c9b7bd8399d736bfcd3d304cdd646145a35
Claudio Lepin <azratul@gmail.com>
2018-10-09 12:27:57 -0300

    Major changes in the way of detection ("play" function)

--- lock-pc.py
+++ lock-pc.py
@@ -10,21 +10,26 @@ import datetime
 target_address = None
 lock_cmd       = ["loginctl lock-session", "gnome-screensaver-command --lock"]
 unlock_cmd     = ["loginctl unlock-session", "gnome-screensaver-command -d"]
+total_services = 0
 
 CMD           = 0 # POR DEFECTO SE UTILIZA EL PRIMER COMANDO DE LA LISTA("lock_cmd", "unlock_cmd")
 SLEEP_TIME    = 1
-DISCOVER_TIME = 5
+DISCOVER_TIME = 4
 DEBUG_MODE    = False
 ARROW         = '➔'
 
 def scan():
 	try:
-		global target_address
+		global target_address, total_services
 		target_name = raw_input(" Ingrese nombre de dispositivo " + ARROW + " ")
 
 		for bdaddr in bluetooth.discover_devices(duration = DISCOVER_TIME):
+			if DEBUG_MODE == True:
+				print(" [{0}]".format(bdaddr))
+
 			if target_name == bluetooth.lookup_name(bdaddr):
 				target_address = bdaddr
+				total_services = len(bluetooth.find_service(address = bdaddr))
 				break
 	except KeyboardInterrupt:
 		pass
@@ -47,21 +52,21 @@ def settings():
 		pass
 
 def play():
-	check = False
 	state = 1
 	print(" Para volver al menú presionar Ctrl+C... ")
 	try:
 		while True:
-			for bdaddr in bluetooth.discover_devices(duration = DISCOVER_TIME):
-				if bdaddr == target_address:
-					check = True
-					break
-				check = False
+			check    = False
+			device   = bluetooth.lookup_name(target_address, timeout = 20)
+			services = bluetooth.find_service(address = target_address)
+
+			if device != None and len(services) >= total_services:
+				check = True
 
 			event = datetime.datetime.now().strftime("%d-%m-%y %H:%M:%S")
 
 			if DEBUG_MODE == True:
-				print(" [{0}] [{1}]".format(event, check))
+				print(" [{0}] [{1}] [{2}] [{3}]".format(event, check, device, len(services)))
 
 			if check == True:
 				if state == 0:
@@ -79,9 +84,10 @@ def play():
 		pass
 
 def menu():
-	while True:
-		os.system('clear')
-		print('''\033[1;31m
+	try:
+		while True:
+			os.system('clear')
+			print('''\033[1;31m
   ╔══════════════════════════════════╗
   ║ ★ L O C K / U N L O C K   P C  ★ ║
   ╚══════════════════════════════════╝
@@ -91,20 +97,22 @@ def menu():
  \033[1;37m ❸\033[0;37m  CONFIGURAR COMANDOS\033[0;37m
  \033[1;37m ❹\033[0;37m  SALIR\033[0;37m
 ''')
-		if target_address != None:
-			print(' Dispositivo encontrado: {0}'.format(target_address))
+			if target_address != None:
+				print(' Dispositivo encontrado: {0}, Servicios: {1}'.format(target_address, total_services))
 
-		opt = raw_input(" Ingrese Opción " + ARROW + " ")
+			opt = raw_input(" Ingrese Opción " + ARROW + " ")
 
-		if opt == "1":
-			scan()
-		elif opt == "2":
-			play()
-		elif opt == "3":
-			settings()
-		elif opt == "4":
-			print('\033[1;32m¡ H A S T A  L U E G O !\033[0;37m')
-			break
+			if opt == "1":
+				scan()
+			elif opt == "2":
+				play()
+			elif opt == "3":
+				settings()
+			elif opt == "4":
+				print('\033[1;32m¡ H A S T A  L U E G O !\033[0;37m')
+				break
+	except KeyboardInterrupt:
+		pass
 
 if __name__ == "__main__":
 	menu()
